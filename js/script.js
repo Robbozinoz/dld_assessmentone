e29d9fca2b9b37f9c0f9189857c42d2be6affc33
Robbozinoz <30688053+Robbozinoz@users.noreply.github.com>
2018-08-10 15:26:55 +0800

    DLD Assessment One
    This is my Cert IV project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var acc = document.getElementsByClassName("introadd");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
